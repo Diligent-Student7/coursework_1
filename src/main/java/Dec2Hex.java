65242ea0baadece0fea9a3d6557edbe6b6b4c99b
Diligent-Student7 <56159484+Diligent-Student7@users.noreply.github.com>
2019-10-29 19:20:09 +0000

    Fixed bug with arg holding values.

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -2,22 +2,21 @@ class Dec2Hex {
 
     private static int arg1;
     private static String errorMessage;
+    private static String output;
 
     public static void main(String args[]) {
-        if(checkInput(args)) {
-            convertToHex(args);
-        } else {
-            showErrorAndEnd(errorMessage);
-        }
+        convertToHex(args);
+        System.out.println(output);
     }
 
     public static String convertToHex(String args[]){
+        if(!checkInput(args))
+            showErrorAndEnd(errorMessage);
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
         {
@@ -26,7 +25,7 @@ class Dec2Hex {
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
+        output = arg1 + " in hexadecimal representation is : " + hexadecimal;
         return hexadecimal;
     }
 
