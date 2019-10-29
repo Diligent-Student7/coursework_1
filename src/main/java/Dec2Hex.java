ee95a4bedfc6eae3d7bf20d36b003123ca9e1bd7
Diligent-Student7 <56159484+Diligent-Student7@users.noreply.github.com>
2019-10-29 18:58:37 +0000

    Refactored converter to check input.

--- src/main/java/Dec2Hex.java
+++ src/main/java/Dec2Hex.java
@@ -1,29 +1,21 @@
 class Dec2Hex {
 
-    private static int Arg1;
+    private static int arg1;
+    private static String errorMessage;
 
     public static void main(String args[]) {
-        //Refactored by separating into another method so it is easier to test.
-        convertToHex(args);
+        if(checkInput(args)) {
+            convertToHex(args);
+        } else {
+            showErrorAndEnd(errorMessage);
+        }
     }
 
     public static String convertToHex(String args[]){
-        if (args.length > 0)
-        {
-            try
-            {
-                Arg1 = Integer.parseInt(args[0]);
-            }
-            catch (NumberFormatException e)
-            {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        }
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
-        num = Arg1;
+        num = arg1;
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
@@ -36,5 +28,28 @@ class Dec2Hex {
 
         System.out.println("Hexadecimal representation is : " + hexadecimal);
         return hexadecimal;
+    }
+
+    static boolean checkInput(String args[]){
+        if (args.length > 0) {
+            try
+            {
+                arg1 = Integer.parseInt(args[0]);
+                return true;
+            }
+            catch (NumberFormatException e)
+            {
+                errorMessage = "Argument '" + args[0] + "' must be an integer.";
+                return false;
+            }
+        }else{
+            errorMessage = "And Input must be given to convert";
+            return false;
+        }
+    }
+
+    static void showErrorAndEnd(String message){
+        System.err.println(message);
+        System.exit(1);
     }
 }
