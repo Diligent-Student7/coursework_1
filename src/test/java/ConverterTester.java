8e23b9e5976dc6768d8ff06b9581ea17daa6e20e
Diligent-Student7 <56159484+Diligent-Student7@users.noreply.github.com>
2019-10-29 18:59:50 +0000

    Added a test to test validator

--- src/test/java/ConverterTester.java
+++ src/test/java/ConverterTester.java
@@ -2,14 +2,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConverterTester {
+class ConverterTester {
 
     @Test
-    public void test(){
+    void testConverter(){
         //Control values, used secondary converters to confirm.
         //Run multiple times to confirm accuracy.
         assertEquals("2D", Dec2Hex.convertToHex(new String[]{"45"}));
         assertEquals("80520", Dec2Hex.convertToHex(new String[]{"525600"}));
         assertEquals("1D8B", Dec2Hex.convertToHex(new String[]{"7563"}));
+    }
+
+    @Test
+    void testValidator(){
+        assertFalse(Dec2Hex.checkInput(new String[]{""}));
+        assertFalse(Dec2Hex.checkInput(new String[]{" "}));
+        assertFalse(Dec2Hex.checkInput(new String[]{"Pizza"}));
+        assertTrue(Dec2Hex.checkInput(new String[]{"12"}));
     }
 }
